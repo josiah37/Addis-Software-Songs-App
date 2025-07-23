e01341a1ff4766020cffb851f8974e257cf31061
Your Name <you@example.com>
2025-07-23 05:31:36 +0300

    feat: add Emotion theme and ui components and styling

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./apps/store";
-
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./theme";
 const root = createRoot(document.getElementById("root"));
 root.render(
    <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+         <App />
+      </ThemeProvider>
    </Provider>
 );
